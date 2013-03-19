95cc678d606add4ece4c9fce797a08920e7a38ab
CthuluPriest <Sergey3561@mail.ru>
2013-03-19 15:07:42 +0300

    multithreaded idle server 1.0

--- server/main.c
+++ server/main.c
@@ -6,11 +6,24 @@
 #include <stdlib.h>
 
 pthread_t thrs[10];
+int thr_vacant[10];
 
-void * thr_func(int  p_sock){
-	printf("lol\n");
-	printf("%d",p_sock);
-	printf("\n");	
+struct thr_args{
+	int p_sock;
+	int index;
+};
+
+void * thr_func(void* arg){
+	struct thr_args args;
+	args.p_sock = ((struct thr_args *)arg)->p_sock;
+	args.index = ((struct thr_args *)arg)->index;
+	
+	//printf("lol\n");
+	printf("sock: %d",args.p_sock);
+	printf("\n");
+	printf("index: %d", args.index);	
+	thr_vacant[args.index] = 1;
+	
 }
 
 
@@ -19,12 +32,20 @@ int main(){
 	int listener, sock;
 	struct sockaddr_in addr;
 	pthread_t pid;
+	int i;
+
+	for (i = 0; i<10; i++){
+		thr_vacant[i] = 1;
+	}	
+
+
 	listener = socket(AF_INET, SOCK_STREAM, 0);
 	if(listener < 0)
 	{
 		perror("listener socket fail");
 		exit(1);
 	}
+
 
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(3561);
@@ -38,11 +59,29 @@ int main(){
 
 	listen(listener, 5);
 	sock = 0;
-//	while(1)
-//	{
-		sock = sock +1;
-		pthread_create(&thrs[0], NULL, thr_func, (void*) sock);
-		pthread_join(thrs[0], NULL);
+	while(1)
+	{
+		//sock = sock +1;
+		sock = accept(listener, NULL, NULL);
+		struct thr_args *tmp;
+		tmp = malloc(sizeof(*tmp));
+		tmp->p_sock = sock;
+		
+		i = 0;
+		while(1){
+
+			if (thr_vacant[i])
+			{
+				tmp->index = i;
+				thr_vacant[i] = 0;
+				break;
+			}
+			i = (i+1)%10;
+			
+		}
+	//	printf("%d\n%d\n",tmp.p_sock, tmp.index);
+		pthread_create(&thrs[tmp->index], NULL, thr_func, (void*)tmp);
+	//	pthread_join(thrs[0], NULL);
 		//sock = accept(listener, NULL, NULL);
-//	}
+	}
 }
